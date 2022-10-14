edd06439d1dda231e259697342959d9e755741bc
Zayon Nieves <zayon.nieves1@gmail.com>
2022-10-14 05:35:50 -0400

    tryAgain method to allow user multiple attempts

--- numberGuessingGame.js
+++ numberGuessingGame.js
@@ -25,6 +25,32 @@ const getInput = require('prompt-sync')();
 const randomNum = Math.floor(Math.random() * 101);
 let guess = null;
 
+// checkpoint
+console.log(randomNum);
+
+
+// tryAgain method: ask user if they would like to continue
+function tryAgain() {
+    let stayOn = null;
+
+    while(true) {
+        stayOn = getInput("Would you like to try again? ('y' for yes or 'n' for no) ");
+
+        if((stayOn !== 'y') && (stayOn !== 'n')) {
+            console.log('Invalid input.\n');
+            continue;
+        }
+        else {
+            break;
+        }
+    }
+
+    // checkpoint
+    console.log(stayOn);
+}
+
+
+
  // user input validation
  while(true) {
     guess = Number(getInput('Enter a number between 0 and 100: '));
@@ -42,3 +68,25 @@ let guess = null;
 
  // checkpoint
  console.log(guess);
+
+ // compare user input to random number
+
+ // guess is correct
+ if(guess === randomNum) {
+    console.log(`
+    Your guess: ${guess}
+    The random number: ${randomNum}
+    
+    That is correct!
+    Congratulations! You Win!`);
+ }
+ // guess is too low
+ else if(guess < randomNum) {
+    console.log('That guess is too low! Try Again.');
+    // ask user to try again
+ }
+ // guess is too high
+ else {
+    console.log('That guess is too high! Try Again.');
+    // ask user to try again
+ }
